ccd391b0fa082e38742adc419f286bd925e3241f
bpxzshop-BCOX <bpxzshop@gmail.com>
2026-03-20 21:00:51 +0700

    Fix URL in loadstring for QuantumOnyx.lua

--- Script/blox fruits.lua
+++ Script/blox fruits.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/QuantumOnyx.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/flazhy/QuantumOnyx/main/QuantumOnyx.lua"))()
