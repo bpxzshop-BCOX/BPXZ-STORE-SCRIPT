aaacdfb053d4a72207215aeec7eea50d2d0b9c99
bpxzshop-BCOX <bpxzshop@gmail.com>
2026-03-21 19:19:58 +0700

    Update Prsion-LIfe.lua

--- Script/Prsion-LIfe.lua
+++ Script/Prsion-LIfe.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/god124/Prsion-LIfe/main/a"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/god124/Prsion-LIfe/refs/heads/main/a"))()
