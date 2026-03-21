f39cf543cd221e8c924aad7c052ec9f87ca549d0
bpxzshop-BCOX <bpxzshop@gmail.com>
2026-03-21 13:17:47 +0700

    Create Death-Rail.lua

---
+++ Script/Death-Rail.lua
@@ -0,0 +1,6 @@
+-- ไม่ต้องแปลภาษานะครับ คัดลอกเลยย
+
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/5132beca3ac02bb7f0536fa934905520.lua"))()
+
+-- ใช้อย่างระมัดระวัง!!
+                    
